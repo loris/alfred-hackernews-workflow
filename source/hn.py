48779b684fccf7d5b5ea55c3d47e34a0b81c216d
Loris Guignard <loris.guignard@gmail.com>
2013-01-17 14:48:15 +0100

    Update the workflow to use the new Open Url action + move python code to a separate file

---
+++ source/hn.py
@@ -0,0 +1,38 @@
+import json
+import urllib2
+from xml.etree import ElementTree as ET
+
+
+def get_items(uri):
+    items = []
+    data = json.loads(urllib2.urlopen(uri).read())
+    for item in data['items']:
+        if 'item_id' in item:
+            result = parse_item(item)
+            items.append(result)
+
+    xml = generate_xml(items)
+    return xml
+
+
+def generate_xml(items):
+    xml_items = ET.Element('items')
+    for item in items:
+        xml_item = ET.SubElement(xml_items, 'item')
+        for key in item.keys():
+            if key is 'uid' or key is 'arg':
+                xml_item.set(key, item[key])
+            else:
+                child = ET.SubElement(xml_item, key)
+                child.text = item[key]
+    print ET.tostring(xml_items)
+
+
+def parse_item(item):
+    return {
+        'uid': item['item_id'],
+        'arg': item['url'],
+        'title': item['title'],
+        'subtitle': item['description'],
+        'icon': 'icon.png'
+    }
